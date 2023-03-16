99c06ad69eb5e67d05c97eb4650ce9bada1ebbcd
Nicholas Poehlman <nicholas.poehlman343@gmail.com>
2023-03-16 07:35:25 -0700

    finished the exersise

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstname="Nicholas";
+let lastname="Poehlman";
+var age="23";
 // Exercise 2
-
+age=23;
+lastname="Poehlman";
+firstname="Nicholas";
 // Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensitive=true;
+
+let price=19.99;
+let isOnsale=false;
+let salePercentage=15;
+let stock=0;
+let instock=false;
+let selectedSize="M";
+
+let title="name of the wind";
+let author="patrick rothloss";
+let pagecount=722;
+let bookmark=456;
+let hasRead=true;
